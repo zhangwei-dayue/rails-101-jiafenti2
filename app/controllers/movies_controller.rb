d1ed3306ee005cd2f63ee786a9648131e475bde4
张伟 <zhangwei19890518@gmail.com>
2017-03-30 13:25:08 +0800

    add permission check on show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :show, :update, :destroy]
+  before_action :find_movie_check_permission , only: [:edit, :update, :destroy]
   def index
     @movies = Movie.all
   end
@@ -9,7 +10,6 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params[:id])
   end
 
   def show
@@ -27,7 +27,6 @@ class MoviesController < ApplicationController
   end
 
   def update
-    @movie = Movie.find(params[:id])
     if @movie.update(movie_params)
       redirect_to movies_path, notice: "编辑电影成功！"
     else
@@ -36,12 +35,18 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params[:id])
     @movie.destroy
     redirect_to movies_path, alert: "删除电影成功！"
   end
 
   private
+
+  def find_movie_check_permission
+    @movie = Movie.find(params[:id])
+    if current_user != @movie.user
+      redirect_to movies_path, alert: "你没有权限进行操作！"
+    end
+  end
 
   def movie_params
     params.require(:movie).permit(:filmname, :introduction)
