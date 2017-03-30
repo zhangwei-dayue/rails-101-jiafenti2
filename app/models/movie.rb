345e0c0451f220840d7d3318e1c29428bed36cd2
张伟 <zhangwei19890518@gmail.com>
2017-03-30 13:08:07 +0800

    show movie's creator info

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,4 @@
 class Movie < ApplicationRecord
   validates :filmname, presence: true
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330033050) do
+ActiveRecord::Schema.define(version: 20170330045840) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "filmname"
     t.text     "introduction"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
