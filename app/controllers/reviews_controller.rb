2866f470a69c7245db27536b2482ae91a69d7364
张伟 <zhangwei19890518@gmail.com>
2017-03-30 13:53:09 +0800

    implement review create function

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, :only => [:new, :edit, :create, :update, :destroy]
+  def new
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.new
+  end
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.new(review_params)
+    @review.movie = @movie
+    @review.user = current_user
+    if @review.save
+      redirect_to movie_path(@movie), notice: "添加影评成功！"
+    else
+      render :new
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
+  validates :content, presence: true
   belongs_to :user
   belongs_to :movie
 end
